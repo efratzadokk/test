5e9ea76e92edd844fae7f7e6d69acd64bd12cf46
atara6316 <fr5836316@gmail.com>
2020-12-17 16:54:06 +0200

    upload multiple image

--- controller/Files.js
+++ controller/Files.js
@@ -11,54 +11,6 @@ upload = async (req, res) => {
     else
         res.status(200).send(null);
 }
-
-// (uploadedMultipleFiles = (fileToUpload, uId, headers) => {
-//     return new Promise(async (resolve, reject) => {
-//         console.log("in uploadedMultipleFiles")
-//         const uri = `https://files.knowme.page/api/${uId}/uploadMultipleFiles`;
-//         console.log("files:", Object.keys(fileToUpload));
-//         console.log("files value:", fileToUpload["galleries"]);
-
-//         let formdata = {}
-//         Object.keys(fileToUpload).forEach(file => {
-//             let currentFile =
-//             {
-//                 [file]: {
-//                     value: fileToUpload[file].data,
-//                     options: {
-//                         filename: fileToUpload[file].name
-//                     },
-//                 }
-//             }
-
-//             formdata = Object.assign(currentFile, formdata);
-//         });
-//         console.log("formdata", formdata);
-//         const options = {
-//             method: "POST",
-//             url: uri,
-//             headers: {
-//                 Authorization: headers,
-//                 "Content-Type": "multipart/form-data",
-//             },
-//             formData: formdata
-//         }
-//         request(options, async (err, res, body) => {
-//             if (err) {
-//                 console.log(err);
-//                 reject(err);
-//             }
-//             console.log("result from server", body);
-//             try {
-//                 console.log("sucsses");
-//                 const files = JSON.parse(body).filesData;
-//                 resolve(files);
-//             } catch (error) {
-//                 reject(error);
-//             }
-//         });
-//     });
-// });
 (uploadedMultipleFiles = (fileToUpload, uId, headers) => {
     return new Promise(async (resolve, reject) => {
         console.log("in uploadedMultipleFiles")
@@ -70,18 +22,17 @@ upload = async (req, res) => {
             let currentFile;
             if (Array.isArray(fileToUpload[file])) {
                 let fileInFileToUpload = fileToUpload[file];
-                fileInFileToUpload.forEach((item,index) => {
-                    currentFile =
-                    {
-                        [file+"_"+index]: {
-                            value: item.data,
-                            options: {
-                                filename: item.name
-                            },
-                        }
-                    }
-                    console.log("@@@@@@@@@@@@@@@@@@@@22",currentFile)
-                    formdata = Object.assign(currentFile, formdata);
+                fileInFileToUpload.forEach((item) => {
+                    if (!formdata.hasOwnProperty(file))
+                        formdata = { ...formdata, ...{ [file]: [] } }                    // formdata = { ...formdata, ...{ [file]: [] } }
+                    formdata[file].push({
+                        value: item.data,
+                        options: {
+                            filename: item.name
+                        },
+                    })
+                    console.log("@@@@@@@@@@@@@@@@@@@@22", formdata)
+                    // formdata = Object.assign(currentFile, formdata);
                 })
             }
             else {
@@ -157,7 +108,7 @@ updateFiles = async (req, res) => {//update post
         const options = {
             url: `https://files.knowme.page/api/${req.params.uId}/updateMultiFilesDB`,
             method: "POST",
-            json: {files,urls },
+            json: { files, urls },
             headers: {
                 Authorization: req.headers["authorization"],
             }
@@ -183,7 +134,7 @@ updateFiles = async (req, res) => {//update post
                     };
                 });
             };
- 
+
         });
     }
     else {
