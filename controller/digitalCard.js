e217e8f0e477e3a520bffd04ba79dec848466a68
sarakorb <sarakorb@leader.codes>
2021-01-12 20:05:31 +0200

    fix url again to username/cardname

--- controller/digitalCard.js
+++ controller/digitalCard.js
@@ -33,13 +33,10 @@ getDigitalCard = async (req, res) => {
 
 getCardById =async (req, res) => {
 
-    // let cardName=req.params.cardName.split("_").join(" ");
-    let cardId=req.params.cardId;
+    let cardName=req.params.cardName.split("_").join(" ");
+    // let cardId=req.params.cardId;
 
-    console.log("cardId",cardId)
-    // let currentUser=await User.findOne({username:req.params.userName})
-    // let _id=currentUser._id;
-    Card.findOne({_id: cardId, isDelete:false})
+    Card.findOne({cardName: cardName, isDelete:false})
     .populate({path:'userId', match:{userName:req.params.userName}})
     .populate({path: "socialMedias"})
     .populate({path: 'gallery'})
--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,6 @@ router.post("/:userName/:cardId/delete", digitalCardController.deleteCard);
 router.get('/:userName/getUser/user', UserController.getUidByUserName);
 router.put("/:userName/:cardId", digitalCardController.updateDigitalCard);
 router.get('/:userName', digitalCardController.getDigitalCard);
-router.get("/:cardName/:cardId", digitalCardController.getCardById);
+router.get("/:userName/:cardName", digitalCardController.getCardById);
 router.post('/:userName', digitalCardController.createDigitalCard);
 module.exports = router;
