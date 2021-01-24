3f9d0c23ef7afffad08a9dc7b76467559793d2a3
sarakorb <sarakorb@leader.codes>
2021-01-24 10:40:04 +0200

    1

---
+++ build/precache-manifest.7b02953ee081f9a614bc52443c13331c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ea2c58bea977beaaadfc1fc7816c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c2ea3751b995ce6abd",
+    "url": "/static/css/2.cfed7de4.chunk.css"
+  },
+  {
+    "revision": "f50356ca39da3d4d0146",
+    "url": "/static/css/main.3021a05b.chunk.css"
+  },
+  {
+    "revision": "11c2ea3751b995ce6abd",
+    "url": "/static/js/2.e54c03ae.chunk.js"
+  },
+  {
+    "revision": "964a0c6da692057f8ac753e6f3002adf",
+    "url": "/static/js/2.e54c03ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50356ca39da3d4d0146",
+    "url": "/static/js/main.df35f65a.chunk.js"
+  },
+  {
+    "revision": "e0e738932c942a311f54",
+    "url": "/static/js/runtime-main.ea1b7941.js"
+  },
+  {
+    "revision": "07df11542b7bef9cc5dc9309c58a06e1",
+    "url": "/static/media/Bitmap.07df1154.png"
+  },
+  {
+    "revision": "95887dee1ba59a214cca5bd2bf10b129",
+    "url": "/static/media/Group 17167@2x.95887dee.png"
+  },
+  {
+    "revision": "9f9081d169f8136e2391e6404f7d36f4",
+    "url": "/static/media/Rectangle.9f9081d1.png"
+  },
+  {
+    "revision": "415d937361a9aa664bef28e05572782b",
+    "url": "/static/media/agentPic.415d9373.png"
+  },
+  {
+    "revision": "cf7c2e481eb1ae64ba81a6649d4684ef",
+    "url": "/static/media/defualtUploadImage.cf7c2e48.svg"
+  },
+  {
+    "revision": "102491b15102a3457ab9c80911dc0701",
+    "url": "/static/media/footerErr.102491b1.png"
+  },
+  {
+    "revision": "11624801bd67f6dd8a3d759eeaeea751",
+    "url": "/static/media/logo2.11624801.PNG"
+  },
+  {
+    "revision": "a6fb01d8742e5e72cf243e94c87fd541",
+    "url": "/static/media/logoPerson.a6fb01d8.png"
+  },
+  {
+    "revision": "7cf1ed2806df83b39692cbcc41b63456",
+    "url": "/static/media/person.7cf1ed28.png"
+  },
+  {
+    "revision": "6b8d0f4a4bcf0f714c5b63c576b8ea61",
+    "url": "/static/media/profile.6b8d0f4a.PNG"
+  }
+]);
