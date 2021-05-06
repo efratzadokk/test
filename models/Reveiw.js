fd7b619455e9bb440d1e713faf8fa47f62b479e4
avitalKalev <avital@leader.codes>
2021-05-06 12:36:50 +0300

    avital pull server

--- models/Reveiw.js
+++ models/Reveiw.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
 const reveiwSchema = mongoose.Schema({
-    src:{ type: String },
-    srcName:{ type: String },
+    src: { type: String },
+    srcName: { type: String },
     recomandName: { type: String },
-    professoin:{ type: String },
-    reviewContect:{ type: String },
-    textColor:{ type: String },
-    backgroundColor:{ type: String },
-    showProfile: {type: Boolean },
-    showProfessoin: {type: Boolean },
-    isDeleted: {type: Boolean },
+    professoin: { type: String },
+    reviewContect: { type: String },
+    textColor: { type: String },
+    backgroundColor: { type: String },
+    showProfile: { type: Boolean },
+    showProfessoin: { type: Boolean },
+    isDeleted: { type: Boolean },
     textAlign: { type: String },
 
 })
