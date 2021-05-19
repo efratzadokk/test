1aeab3b399cc3b30f6d26c596a12bde4532e0448
sarakorb <sarakorb@leader.codes>
2021-05-19 15:27:46 +0300

    fix grid gallery

--- models/Gallery.js
+++ models/Gallery.js
@@ -7,9 +7,6 @@ const gallerySchema = mongoose.Schema({
     },
     src:{ type: String },
     srcName: { type: String },
-    thumbnail: { type: String },
-    thumbnailWidth: { type: Number },
-    thumbnailHeight: { type: Number },
     caption:{ type: String },
     description:{ type: String },
     isDelete:{ type: Boolean}
