5fa18f238d979e6a7f3a7536d52b1841c9c6056f
faigiLeader <faigi@leader.cods>
2021-06-08 13:26:16 +0300

    template8

--- models/Card.js
+++ models/Card.js
@@ -10,15 +10,15 @@ const cardSchema = mongoose.Schema({
         default: new Date()
     },
     valid: [{ type: String }],
-    listMail:[{ type: String }],
+    listMail: [{ type: String }],
     isDelete: { type: Boolean },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
     cardName: { type: String, required: true },
-    lastTenColor:[{type: String }],
-    marketMember:{ type: Boolean },
+    lastTenColor: [{ type: String }],
+    marketMember: { type: Boolean },
     fullName: {
         title: { type: String },
         show: { type: Boolean }
@@ -34,7 +34,7 @@ const cardSchema = mongoose.Schema({
             iconShow: { type: Boolean },
             titleShow: { type: Boolean },
             textAlign: { type: Boolean },
-            reverse:  { type: Boolean },
+            reverse: { type: Boolean },
         },
         contact: {
             title: { type: String },
@@ -42,7 +42,7 @@ const cardSchema = mongoose.Schema({
             iconShow: { type: Boolean },
             titleShow: { type: Boolean },
             textAlign: { type: Boolean },
-            reverse:  { type: Boolean },
+            reverse: { type: Boolean },
         }
     },
     socialMediaSpace: {
@@ -91,12 +91,12 @@ const cardSchema = mongoose.Schema({
             title: { type: String },
             show: { type: Boolean }
         },
-        name:{
+        name: {
             title: { type: String },
             show: { type: Boolean },
             require: { type: Boolean }
         },
-        email:{
+        email: {
             title: { type: String },
             show: { type: Boolean },
             require: { type: Boolean }
@@ -233,9 +233,9 @@ const cardSchema = mongoose.Schema({
             }
         },
         buttons: {
-            width:{ type: String},
+            width: { type: String },
             top: { type: String },
-            width:{ type: String },
+            width: { type: String },
             callMe: {
                 background: { type: String },
                 border: { type: String },
@@ -293,6 +293,7 @@ const cardSchema = mongoose.Schema({
                 textAlign: { type: String }
             },
         },
+        buttomEditColor:{ type: String },
         emailForm: {
             background: { type: String },
             title: {
@@ -306,10 +307,10 @@ const cardSchema = mongoose.Schema({
                 textAlign: { type: String },
                 background: { type: String },
             },
-            input:{
-                background:{ type: String },
-                textAlign:{ type: String },
-                color:{ type: String },
+            input: {
+                background: { type: String },
+                textAlign: { type: String },
+                color: { type: String },
             }
         },
         videoStyle: {
@@ -337,13 +338,13 @@ const cardSchema = mongoose.Schema({
         color: { type: String },
         fade: { type: Boolean },
         autoPlay: { type: Number },
-        dots:{ type: Boolean },
+        dots: { type: Boolean },
         margin: { type: Number },
         rowHeight: { type: Number },
         maxRows: { type: Number },
         lightboxGrid: { type: Boolean },
         lightboxSlide: { type: Boolean },
-        direction:{ type: String },
+        direction: { type: String },
     },
     reviewsList: [{
         type: mongoose.Schema.Types.ObjectId,
@@ -360,9 +361,9 @@ const cardSchema = mongoose.Schema({
 });
 
 
-cardSchema.pre('save', function(next){
-    const card=this
-    card.cardName=card.cardName.replace(/[/\s.]/g, '')   
+cardSchema.pre('save', function (next) {
+    const card = this
+    card.cardName = card.cardName.replace(/[/\s.]/g, '')
     next()
 })
 
