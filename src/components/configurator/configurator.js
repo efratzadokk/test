548ae28a29070773f62ecb5a94ae3ea368be05f9
salomee-a <salomee@leader.codes>
2021-06-23 22:12:31 +0300

    salomee configurator

--- src/components/configurator/configurator.js
+++ src/components/configurator/configurator.js
@@ -1,46 +1,28 @@
+
 // import React, { useEffect, useState } from 'react';
 // import { connect } from 'react-redux';
 // import './configurator.css'
 // import EditPage from '../editPage/editPage'
 // import EditSection from '../editSection/editSection'
 // import EditElement from '../editElement/editElement'
-// import { chageEditMode, creatFunnel, getAllFunnel, updateFunnel, updateFunnel1,removeFunnel, setFlagAllFunnnels, setAllFunnels, uploadFile, save, sectionInEditing, setCollapseOpen } from '../../redux/actions/funnel.action'
+// import { chageEditMode, creatFunnel, getAllFunnel, updateFunnel, updateFunnel1,removeFunnel, setFlagAllFunnnels, setAllFunnels, uploadFile, save, sectionInEditing, setCollapseOpen,setMonIndex } from '../../redux/actions/funnel.action'
 // import {  Route, withRouter } from 'react-router-dom'
 // import html2canvas from 'html2canvas';
+// import Grid  from '@material-ui/core/Grid';
+// import Grids from '../grids/grids';
 
+// import {FiSettings} from 'react-icons/fi'
+// import {CgScreen} from 'react-icons/cg'
+// import {BsEye} from 'react-icons/bs'
 
 // function Configurator(props) {
 
 //     const [flag, setFlag] = useState(false);
-//     const { history, setFlagAllFunnnels, userName, funnelName, flagCon, collapseIsOpen, elementInEditing, chageEditMode, jsonPage, creatFunnel, getAllFunnel, save, updateFunnel, removeFunnel, idPage, nameFunnel, nameUser, uploadFile, urlPage, sectionInEditing, setCollapseOpen } = props
-
-//     //////////אני  יירקתי
-//     // useEffect(() => {
-
-//     //     window.addEventListener("message", messageFromIframe, false);
-//     // })
-
-//     //////////אני  יירקתי
-// //     function messageFromIframe(event) {
-// //         console.log('I am an iframe, I accepted it:', event.data);
-// //         let data = JSON.parse(event.data)
-// //         // console.log(data.params)
-// //         switch (data.function) {
-// //             case 'set collapse open':
-// //                 console.log(data.params)
-// // debugger
-// //                 sectionInEditing(data.params.id)//data.params.num
-// //                 setCollapseOpen({collapse:data.params.elementOrSection,id:data.params.id})
-
-// //                 break;
-
-// //         }
-// //     }
-
-//     // let color = elementInEditing.settings.style.color
-//     function changeColor(co) {
-//         elementInEditing.settings.style.color = co;
-//     }
+//     const { history, setFlagAllFunnnels, userName, funnelName, flagCon, collapseIsOpen, elementInEditing, chageEditMode, jsonPage, creatFunnel, getAllFunnel, save, updateFunnel, removeFunnel, idPage, nameFunnel, nameUser, uploadFile, urlPage, sectionInEditing, setCollapseOpen ,setMonIndex,monindex} = props
+  
+//     // function changeColor(co) {
+//     //     elementInEditing.settings.style.color = co;
+//     // }
 //     const clickAllFunnels = () => {
 //         if (window.location.pathname.includes('all'))
 //             history.push(`/${userName}/${funnelName}`)
@@ -64,16 +46,11 @@
 //             let imgData = canvas.toDataURL();
 //             console.log(imgData);
 //             var funnelImage = dataURLtoFile(imgData, 'event.png');
-//             // console.log(emailImg);
-//             // this.props.getUrlImage(emailImg);
-//             //this.props.setEmailBody(img);
-//             // let win = window.open();
-//             // win.document.write("<img src='"+imgData+"'/>");
 //             console.log(funnelImage);
 //             const myFile = new FormData()
 //             myFile.append("file", funnelImage[0])
 //             save(funnelImage)
-//             // return (funnelImage)
+     
 //         });
 //     }
 //     const updatefunnel2 = () => {
@@ -83,16 +60,10 @@
 //             let imgData = canvas.toDataURL();
 //             console.log(imgData);
 //             var funnelImage = dataURLtoFile(imgData, 'event.png');
-//             // console.log(emailImg);
-//             // this.props.getUrlImage(emailImg);
-//             //this.props.setEmailBody(img);
-//             // let win = window.open();
-//             // win.document.write("<img src='"+imgData+"'/>");
 //             console.log(funnelImage);
 //             const myFile = new FormData()
 //             myFile.append("file", funnelImage[0])
 //             updateFunnel1(funnelImage)
-//             // return (funnelImage)
 //         });
 //     }
 //     const dataURLtoFile = (dataurl, filename) => {
@@ -108,39 +79,55 @@
 //         return new File([u8arr], filename, { type: mime });
 //     }
 
-//     // const saveOrUpdatefunnel = () => {
-//     //     save(clic())
-//     //     // await uploadFile(clic())
-//     //     //     .then((v) => {
-//     //             // if (v) {
-//     //             // if (idPage)
-//     //             //     updateFunnel()
-//     //             // else
-//     //             //     creatFunnel()
-//     //             // // }
-//     //         // }).catch(e => {
-//     //         //     console.log(e);
-//     //         // });
-//     // }
+//     const changeTable= (v)=>{
+//         debugger
+//   console.log('here')
+//   console.log(monindex)
+//   console.log(v)
+//      setMonIndex(v)
+
+//     }
+
+  
 //     return (
-//         <>
+//         <> 
 //             {  flagCon &&
 //                 <>
 //                     <div id="wrap-configurator" className="pt-4 px-2">
 //                         <div id="head-configurator" className="row d-flex justify-content-between m-0 mb-3 mt-1">
-//                             {/* <input></input> */}
 //                             {/* <span className="material-icons pointer" onClick={() => clickAllFunnels()}>
 //                             settings
 //                  </span> */}
 //                             {/* <Link to={window.location.pathname.includes('all')?`/${userName}/${funnelName}`:`/${userName}/${funnelName}/all`}> */}
-//                             <span className="material-icons pointer" onClick={() => clickAllFunnels()}>
-//                                 settings
-//                  </span>
-                            
+                      
 //                         </div>
-//                         <div id="accordion" className=''>
-//                             <EditPage></EditPage>
-//                             {(() => {
+                       
+
+
+//                         <div id="accordion" style={{height:'100%'}}>
+//                             {/* <EditPage></EditPage> */}
+
+
+//                             <Grid container direction="row" style={{height:"13px", color:'grey',textAlign:'center', fontSize:'15px'}}>
+
+                          
+//                                  {/* Design */}
+//                                 {/* <div className="mb" onClick={changeTable(1)}> Design</div>   */}
+//                                 {/* </Grid>
+//                             <Grid item sm={6} md={6}>  */}
+//                             {/* Grid */}
+//                                  {/* <div className="mb" onClick={changeTable(0)}>Grid</div>
+//                             </Grid> */} 
+//                               <Grid item sm={6} md={6}> <button  id={monindex?'bl':''} className='b1' onClick={()=>changeTable(1)}>Design</button> </Grid>
+//                               <Grid item sm={6} md={6} > <button  id={monindex?'':'bl'} className='b2' onClick={()=>changeTable(0)}>Grid</button> </Grid>
+                                
+//                             </Grid>
+//                               <Grid container direction="row" style={{height:"20px"}}></Grid>
+                         
+//                          <Grid container direction ='row' style={{height:'60%'}} >
+//                             { monindex ?
+//                            (() => {
+                               
 //                                 switch (collapseIsOpen) {
 //                                     case 'section':
 //                                         return <EditSection></EditSection>
@@ -149,27 +136,43 @@
 //                                     default:
 //                                         break;
 //                                 }
-//                             })()}
-//                             {/* <button onClick={() => clic()}> click</button> */}
-//                             <Route render={({ history }) => (
-//                                 <div id="bottom_configurtor">
-//                                     <button className='btn' onClick={() => clickView(history)}>
-//                                         <span class="material-icons iconBottom" style={{ color: "#cac6c6" }}>
-//                                             remove_red_eye
-//                             </span>
-//                                     </button>
-//                                     {idPage && <> <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => updatefunnel2()} >update</button>
-//                                         <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => removeFunnel("5fd9f7b28eedc0eb8b5dea30")} >remove</button></>
-//                                         ||
-//                                         <button className='btn' id="whatapp" style={{ background: '#ff5f5f' }} onClick={() => saveOrUpdatefunnel()} >save</button>}
+//                             } )() 
+//                         : <Grids/>
 
-//                                 </div>
+
+                           
+//         }
+//         <Route render={({ history }) => (
+//             <div id="bottom_configurtor"  >
+//                 <button className='btn' onClick={() => clickView(history)}>
+//                     <CgScreen  style={{ color: "#cac6c6" }}></CgScreen>
+//                 </button>
+//                 <button className='btn' onClick={() => clickView(history)}>
+//             <BsEye style={{ color: "#cac6c6" }}></BsEye>    
+//                 </button>
+
+//                 <button className='btn' onClick={() => clickView(history)}>
+//                     <FiSettings  style={{ color: "#cac6c6" }}></FiSettings>
+//                 </button>
+//                 {idPage && <div> <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => updatefunnel2()} >update</button>
+//                     <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => removeFunnel("5fd9f7b28eedc0eb8b5dea30")} >remove</button>
+//                     </div>
+//                     ||
+//                     // <button className='btn '  style={{width:'80%', backgroundColor:'blue'}} onClick={() => saveOrUpdatefunnel()} >save</button>}
+//                     <button type="button" id="whatapp" class="btn btn-info btn-sm" onClick={() => saveOrUpdatefunnel()}>update</button>
+//         }
+//             </div>
+                         
+
+
+                           
                                 
 
 
 
 
 //                             )} />
+//                                </Grid>
 //                         </div>
 //                     </div>
 //                 </>
@@ -192,6 +195,7 @@
 //             userName: state.user.userName,
 //             funnelName: state.funnel.name,
 //             // iframe: state.funnel.iframe,
+//             monindex:state.funnel.monindex
 //         }
         
 //     },
@@ -207,12 +211,11 @@
 //             uploadFile: (file) => { dispatch(uploadFile(file)) },
 //             save: (file) => { dispatch(save(file)) },
 //             setCollapseOpen: function (collapse) { dispatch(setCollapseOpen(collapse)) },
-//             sectionInEditing: (indexSection) => { dispatch(sectionInEditing(indexSection)) }
+//             sectionInEditing: (indexSection) => { dispatch(sectionInEditing(indexSection)) },
+//             setMonIndex: (idx)=>{dispatch(setMonIndex(idx))},
 //         }
 //     }
 // )(Configurator))
-
-
 
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
@@ -225,10 +228,6 @@ import {  Route, withRouter } from 'react-router-dom'
 import html2canvas from 'html2canvas';
 import Grid  from '@material-ui/core/Grid';
 import Grids from '../grids/grids';
-
-import {FiSettings} from 'react-icons/fi'
-import {CgScreen} from 'react-icons/cg'
-import {BsEye} from 'react-icons/bs'
 
 function Configurator(props) {
 
@@ -303,6 +302,7 @@ function Configurator(props) {
 
     }
 
+    console.log(monindex)
   
     return (
         <> 
@@ -310,36 +310,28 @@ function Configurator(props) {
                 <>
                     <div id="wrap-configurator" className="pt-4 px-2">
                         <div id="head-configurator" className="row d-flex justify-content-between m-0 mb-3 mt-1">
-                            {/* <span className="material-icons pointer" onClick={() => clickAllFunnels()}>
-                            settings
-                 </span> */}
-                            {/* <Link to={window.location.pathname.includes('all')?`/${userName}/${funnelName}`:`/${userName}/${funnelName}/all`}> */}
-                      
+                                 <span className="material-icons pointer" onClick={() => clickAllFunnels()}>
+                                settings
+                 </span>
+                            
                         </div>
                        
 
 
                         <div id="accordion" style={{height:'100%'}}>
-                            {/* <EditPage></EditPage> */}
-
-
-                            <Grid container direction="row" style={{height:"13px", color:'grey',textAlign:'center', fontSize:'15px'}}>
+                           
+                            <Grid container direction="row" style={{height:"6vh", color:'grey',textAlign:'center', fontSize:'15px'}}>
 
                           
-                                 {/* Design */}
-                                {/* <div className="mb" onClick={changeTable(1)}> Design</div>   */}
-                                {/* </Grid>
-                            <Grid item sm={6} md={6}>  */}
-                            {/* Grid */}
-                                 {/* <div className="mb" onClick={changeTable(0)}>Grid</div>
-                            </Grid> */} 
-                              <Grid item sm={6} md={6}> <button  id={monindex?'bl':''} className='b1' onClick={()=>changeTable(1)}>Design</button> </Grid>
-                              <Grid item sm={6} md={6} > <button  id={monindex?'':'bl'} className='b2' onClick={()=>changeTable(0)}>Grid</button> </Grid>
+                                
+                              <Grid item sm={6} md={6}> <button  id={monindex?'bl':''} className='b1' onClick={()=>changeTable(1)} style={{fontSize:"1.5vw"}}>Design</button> </Grid>
+                              <Grid item sm={6} md={6} > <button  id={monindex?'':'bl'} className='b2' onClick={()=>changeTable(0)} style={{fontSize:"1.5vw"}}>Grid</button> </Grid>
                                 
                             </Grid>
-                              <Grid container direction="row" style={{height:"20px"}}></Grid>
+                              
                          
-                         <Grid container direction ='row' style={{height:'60%'}} >
+                             <Grid container direction ='row'id='gdm' style={{height:'80%',marginTop:'4%'}} >
+                                 
                             { monindex ?
                            (() => {
                                
@@ -355,32 +347,36 @@ function Configurator(props) {
                         : <Grids/>
 
 
-                           
-        }
-        <Route render={({ history }) => (
-            <div id="bottom_configurtor"  >
-                <button className='btn' onClick={() => clickView(history)}>
-                    <CgScreen  style={{ color: "#cac6c6" }}></CgScreen>
-                </button>
-                <button className='btn' onClick={() => clickView(history)}>
-            <BsEye style={{ color: "#cac6c6" }}></BsEye>    
-                </button>
-
-                <button className='btn' onClick={() => clickView(history)}>
-                    <FiSettings  style={{ color: "#cac6c6" }}></FiSettings>
-                </button>
-                {idPage && <div> <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => updatefunnel2()} >update</button>
-                    <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => removeFunnel("5fd9f7b28eedc0eb8b5dea30")} >remove</button>
-                    </div>
-                    ||
-                    // <button className='btn '  style={{width:'80%', backgroundColor:'blue'}} onClick={() => saveOrUpdatefunnel()} >save</button>}
-                    <button type="button" id="whatapp" class="btn btn-info btn-sm" onClick={() => saveOrUpdatefunnel()}>update</button>
-        }
-            </div>
+                            }
                          
 
 
                            
+                            <Route render={({ history }) => (
+                                <div id="bottom_configurtor"   >
+                                     <Grid container direction="row" style={{height:"100%", width:'100%'}} >
+                                     <Grid item md={2} sm={2}><button className='btn' style={{width:'100%',height:'100%',textAlign:'center'}} onClick={() => clickView(history)}>
+                                        <span class="material-icons iconBottom" style={{ color: "#cac6c6"}}>
+                                            remove_red_eye
+                                        </span>
+                                         </button>
+                                        </Grid>
+                                        <Grid item md={2} sm={2}></Grid>
+                                    <Grid item md={5} sm={5}>
+                                    {idPage && <div> <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => updatefunnel2()} >update</button>
+                                        <button className='btn' style={{ background: '#ff5f5f' }} onClick={() => removeFunnel("5fd9f7b28eedc0eb8b5dea30")} >remove</button>
+                                        </div>
+                                        ||
+                                        // <button className='btn '  style={{width:'80%', backgroundColor:'blue'}} onClick={() => saveOrUpdatefunnel()} >save</button>}
+                                        <button type="button" id="whatapp"  onClick={() => saveOrUpdatefunnel()}>Save</button>
+                            }
+                                        </Grid>
+                                        <Grid item md={2} sm={2}></Grid>
+
+                                     </Grid>
+                                    
+                                    
+                                </div>
                                 
 
 
@@ -431,4 +427,5 @@ export default withRouter(connect(
         }
     }
 )(Configurator))
+
 
